a8ac1d4adc41e834184676a8970236d36a207928
Sean Moore <sean.m.moore@gmail.com>
2021-05-04 15:18:36 -0700

    Add Rank component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = () => {
             </p>
             <div className='form center pa4 br3 shadow-5'>
                 <input className='f4 pa2 w-70 center' type="text" />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple bn'>Detect</button>
             </div>
         </div>
     );

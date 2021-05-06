f3641d74d9b26c044988f8f1bec2798afb9f5866
Sean Moore <sean.m.moore@gmail.com>
2021-05-06 09:28:33 -0700

    Update ImageLinkForm prompt

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className='f3'>
-                {'This magic brain will detect faces in your pictures. Give it a try.'}
+                {'This magic brain will detect faces in pictures. Try an image URL!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
